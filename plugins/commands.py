05571eb670786eccc3271af2b3e5a9b96e574f5b
monitg <142651228+monitg@users.noreply.github.com>
2025-01-03 11:12:02 +0530

    Update commands.py
    strikers changed

--- plugins/commands.py
+++ plugins/commands.py
@@ -76,7 +76,7 @@ async def start(client, message):
         if CLONE_MODE == True:
             buttons.append([InlineKeyboardButton('🤖 Cʀᴇᴀᴛᴇ Yᴏᴜʀ Oᴡɴ Cʟᴏɴᴇ Bᴏᴛ 🤖', callback_data='clone')])
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgUAAxkBAAENnUFlrTIf0FT-TgqEaSAhVh_UQKWFiQACFgoAAoBu0VSY_9b1MN-k_R4E") 
+        m=await message.reply_sticker("CAACAgUAAxkBAAEBT3dnd2JkCu3rJKp0jF9-eu21f8kOtwACRRIAAsPvkVREHw5SlTxKtB4E") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
@@ -220,7 +220,7 @@ async def start(client, message):
             if CLONE_MODE == True:
                 buttons.append([InlineKeyboardButton('🤖 Cʀᴇᴀᴛᴇ Yᴏᴜʀ Oᴡɴ Cʟᴏɴᴇ Bᴏᴛ 🤖', callback_data='clone')])
             reply_markup = InlineKeyboardMarkup(buttons)
-            m=await message.reply_sticker("CAACAgUAAxkBAAENnUFlrTIf0FT-TgqEaSAhVh_UQKWFiQACFgoAAoBu0VSY_9b1MN-k_R4E") 
+            m=await message.reply_sticker("CAACAgUAAxkBAAEBT3dnd2JkCu3rJKp0jF9-eu21f8kOtwACRRIAAsPvkVREHw5SlTxKtB4E") 
             await asyncio.sleep(1)
             await m.delete()
             await message.reply_photo(
@@ -1479,7 +1479,7 @@ async def check_plans_cmd(client, message):
             [InlineKeyboardButton("⚠️ ᴄʟᴏsᴇ / ᴅᴇʟᴇᴛᴇ ⚠️", callback_data="close_data")]
         ]
         reply_markup = InlineKeyboardMarkup(btn)
-        m=await message.reply_sticker("CAACAgUAAxkBAAENnUFlrTIf0FT-TgqEaSAhVh_UQKWFiQACFgoAAoBu0VSY_9b1MN-k_R4E")         
+        m=await message.reply_sticker("CAACAgUAAxkBAAEBT3dnd2JkCu3rJKp0jF9-eu21f8kOtwACRRIAAsPvkVREHw5SlTxKtB4E")         
         await message.reply_text(f"**😢 You Don't Have Any Premium Subscription.\n\n Check Out Our Premium /plan**",reply_markup=reply_markup)
         await asyncio.sleep(2)
         await m.delete()
